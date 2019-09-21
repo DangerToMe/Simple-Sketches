e04f08de9e69805768583b95d591f13182ee3363
DangerToMe <55562002+DangerToMe@users.noreply.github.com>
2019-09-21 09:36:44 -0400

    Update misc_Random_blink_multiple_LEDs_noDelay.ino

--- LED Related/misc_Random_blink_multiple_LEDs_noDelay.ino
+++ LED Related/misc_Random_blink_multiple_LEDs_noDelay.ino
@@ -1,16 +1,14 @@
-//Blinks a set amount of LEDs chosen at random
+//Blinks a set amount of LEDs chosen at random from 6 pins
 //with random blink speeds
 
-//Has a bug. Occasionally, it only blinks one LED. Not sure what
-//that's all about. But I'll revisit this script later.
-
 int ledPins[] = {3, 5, 6, 7, 8, 9};
-int blinkHowMany = 2;
+int numToBlink = 2;
 int ledsChosen = 0;
 int selectedPins[] = {0, 0, 0, 0, 0, 0};
 int ledState[] = {LOW, LOW, LOW, LOW, LOW, LOW};
 int getNew = 0;
-int arrayIndex = 0;
+int pinIndex = 0;
+int timeIndex = 0;
 
 unsigned long pickRandom = 2000; //milliseconds between reselecting random pins and blink times
 unsigned long randomMillis;
@@ -19,62 +17,69 @@ unsigned long previousMillis[] = {0, 0, 0, 0, 0, 0};
 unsigned long selectedTimes[] = {0, 0, 0, 0, 0, 0};
 
 void setup() {
+  selectRandomPins();
+  selectRandomTimes();
   for (int i = 0; i < 6; i++) {
     pinMode(ledPins[i], OUTPUT);
   }
 }
 
 void loop() {
+  checkSelectionInterval();
+}
+
+void selectRandomPins() {
+  while (pinIndex < numToBlink) {
+    getNew = 0;
+    int randomPin = ledPins[random(0, 6)];
+    for (int i = 0; i < numToBlink; i++) {
+      if (randomPin == selectedPins[i]) {
+        getNew = 1;
+      }
+    }
+    if (getNew == 0) {
+      selectedPins[pinIndex] = randomPin;
+      pinIndex++;
+    }
+  }
+}
+
+void selectRandomTimes() {
+  timeIndex = 0;
+  while (timeIndex < numToBlink) {
+    unsigned long randomTime = blinkTimes[random(0, 4)];
+    selectedTimes[timeIndex] = randomTime;
+    timeIndex++;
+  }
+}
+
+void checkSelectionInterval() {
+  unsigned long currentMillis = millis();
+  if ((currentMillis - randomMillis) >= pickRandom) {
+    randomMillis = currentMillis;
+    for (int i = 0; i < 6; i++) {
+      digitalWrite(ledPins[i], LOW);
+      ledState[i] = LOW;
+    }
+    pinIndex = 0;
+    selectRandomPins();
+    selectRandomTimes();
+  }
   blinkLEDs();
 }
 
-void chooseLEDs() {
-  int randomLED = ledPins[random(0, 6)];
-  for (int i = 0; i < blinkHowMany; i++) {
-    if (randomLED == selectedPins[i]) {
-      getNew = 1;
-    }
-  }
-  if (getNew == 1) {
-    blinkLEDs();
-  }
-  if (getNew == 0) {
-    selectedPins[arrayIndex] = randomLED;
-    int randomTime = blinkTimes[random(0, 4)];
-    selectedTimes[arrayIndex] = randomTime;
-    arrayIndex++;
-    if (arrayIndex == blinkHowMany) {
-      arrayIndex = 0;
-    }
-    ledsChosen++;
-  }
-}
-
 void blinkLEDs() {
-  if (ledsChosen < blinkHowMany) {
-    getNew = 0;
-    chooseLEDs();
-  } else {
-    unsigned long currentMillis = millis();
-    if ((currentMillis - randomMillis) >= pickRandom) {
-      randomMillis = currentMillis;
-      for (int i = 0; i < 6; i++) {
-        digitalWrite(ledPins[i], LOW);
+  unsigned long currentMillis = millis();
+  for (int i = 0; i < numToBlink; i++) {
+    if ((currentMillis - previousMillis[i]) >= selectedTimes[i]) {
+      previousMillis[i] = currentMillis;
+      if (ledState[i] == LOW) {
+        ledState[i] = HIGH;
+
+      } else {
         ledState[i] = LOW;
       }
-      ledsChosen = 0;
-      chooseLEDs();
-    }
-    for (int i = 0; i < blinkHowMany; i++) {
-      if ((currentMillis - previousMillis[i]) >= selectedTimes[i]) {
-        previousMillis[i] = currentMillis;
-        if (ledState[i] == LOW) {
-          ledState[i] = HIGH;
-        } else {
-          ledState[i] = LOW;
-        }
-        digitalWrite(selectedPins[i], ledState[i]);
-      }
+      digitalWrite(selectedPins[i], ledState[i]);
     }
   }
 }
