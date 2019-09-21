abf8884da8808c38320a25c51b6e2d118c192f9a
DangerToMe <55562002+DangerToMe@users.noreply.github.com>
2019-09-21 10:41:01 -0400

    Update misc_Random_blink_multiple_LEDs_noDelay.ino

--- LED Related/misc_Random_blink_multiple_LEDs_noDelay.ino
+++ LED Related/misc_Random_blink_multiple_LEDs_noDelay.ino
@@ -9,6 +9,7 @@ int ledState[] = {LOW, LOW, LOW, LOW, LOW, LOW};
 int getNew = 0;
 int pinIndex = 0;
 int timeIndex = 0;
+int arraySize;
 
 unsigned long pickRandom = 2000; //milliseconds between reselecting random pins and blink times
 unsigned long randomMillis;
@@ -17,14 +18,38 @@ unsigned long previousMillis[] = {0, 0, 0, 0, 0, 0};
 unsigned long selectedTimes[] = {0, 0, 0, 0, 0, 0};
 
 void setup() {
+  Serial.begin(9600);
   selectRandomPins();
   selectRandomTimes();
   for (int i = 0; i < 6; i++) {
     pinMode(ledPins[i], OUTPUT);
   }
+  Serial.println("Select Carriage return below to chnge number of LEDs to blink");
+  arraySize = sizeof( ledPins ) / sizeof( int );
 }
 
 void loop() {
+  if ( Serial.available () > 0 ) {
+    static char input[16];
+    static uint8_t i;
+    char c = Serial.read ();
+    if ( c != '\r' && i < 15 ) { // assuming "Carriage Return" is chosen in the Serial monitor as the line ending character
+      input[i++] = c;
+    } else {
+      input[i] = '\0';
+      i = 0;
+      int number = atoi( input );
+      //if number is equal to or greater than the number of LEDs being used,
+      //my Arduino hangs and has to be restarted. So, insure that doesn't happen
+      if (number >= arraySize) {
+        numToBlink = arraySize - 1;
+        Serial.print(numToBlink);
+        Serial.println(" is the maximum allowed.");
+      } else {
+        numToBlink = number;
+      }
+    }
+  }
   checkSelectionInterval();
 }
 
