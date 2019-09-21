da1fba2bf8cb02a2b477679d7f5133f919b9fb1e
DangerToMe <55562002+DangerToMe@users.noreply.github.com>
2019-09-21 14:05:08 -0400

    Add files via upload

---
+++ LED Related/misc_random_led_with_button_press.ino
@@ -0,0 +1,56 @@
+//Select random LED with button press
+
+int randomPin;
+int arraySize;
+int ledState = HIGH;
+int buttonState;
+int lastButtonState = LOW;
+
+const int buttonPin = 11;
+const int ledPins[] = {3, 5, 6, 7, 8, 9}; //pin 4 on my uno is giving odd results
+
+unsigned long debounceDelay = 50;
+unsigned long lastDebounceTime = 0;
+
+void setup() {
+  arraySize = sizeof( ledPins ) / sizeof( int );
+  for (int i = 0; i < arraySize; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
+  randomSeed(analogRead(A0)); //change to an unused analog pin
+  randomPin = ledPins[random(0, arraySize)];
+  digitalWrite(randomPin, HIGH);
+}
+
+void loop() {
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+      // turn off all LEDs and select a new one
+      if (buttonState == HIGH) {
+        //ledState = !ledState;
+        for (int i = 0; i < arraySize; i++) {
+          digitalWrite(ledPins[i], LOW);
+        }
+        randomPin = ledPins[random(0, 6)];
+      }
+    }
+  }
+  // set the LED:
+  digitalWrite(randomPin, HIGH);
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastButtonState = reading;
+}
